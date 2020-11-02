9e5cc016acddfea16010e16b2a527875ac7c2cbc
Jasonzj <925645402@qq.com>
2020-11-02 21:03:30 +0800

    test action

--- src/startup.py
+++ src/startup.py
@@ -1,27 +1,33 @@
+from urllib import request, parse
 from datetime import datetime
 import time
 from login import Login
+from config import USERNAME, PASSWORD, PUSH_KEY
+
+times = 1
 
 def timer(n, task):
     count = 0
     while True: 
         count += 1
-        if (count > 5):
-            print('Not found online class')
+        if (count > times):
+            msg = '没有正在上的课'
+            print('https://sc.ftqq.com/' + PUSH_KEY + '.send?text=' + msg)
+            request.urlopen('https://sc.ftqq.com/' + PUSH_KEY + '.send?text=' + msg)
+            print(msg)
             break
         print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         result = task.enterOnlineClass()
         if (result):
-            print('Attend class success')
+            msg = '进入课堂成功'
+            request.urlopen('https://sc.ftqq.com/' + PUSH_KEY + '.send?text=' + msg)
+            print(msg)
             break
         time.sleep(n)
 
 
-username = ''
-password = ''
-
-task = Login(username=username, password=password)
+task = Login(username=USERNAME, password=PASSWORD)
 task.setBrowser()
 task.login()
-timer(15 * 60, task)
-# timer(5, task)
+# timer(15 * 60, task)
+timer(1, task)
