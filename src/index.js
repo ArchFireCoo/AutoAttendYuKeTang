5f0693e27ac14ed11acd355723fcea76d2431055
arrayJY <ji957455952@163.com>
2021-04-20 14:09:24 +0800

    签到多个课程

--- src/index.js
+++ src/index.js
@@ -1,54 +1,55 @@
-const NodeRSA = require('node-rsa')
-const path = require('path')
-const fs = require('fs')
-const got = require('got')
-const { CookieJar } = require('tough-cookie')
-const sendNotify = require('./sendNotify')
+const NodeRSA = require("node-rsa");
+const path = require("path");
+const fs = require("fs");
+const got = require("got");
+const { CookieJar } = require("tough-cookie");
+const sendNotify = require("./sendNotify");
 
 const resolve = function (...args) {
-  return path.resolve(__dirname, ...args)
-}
+  return path.resolve(__dirname, ...args);
+};
 
-let count = 0
-const times = 900
+let count = 0;
+const times = 900;
 
-const cookieJar = new CookieJar()
+const cookieJar = new CookieJar();
 
 const customGot = got.extend({
   cookieJar,
-})
+});
 
 const successLessons = new Set();
 
-const publicKey = fs.readFileSync(resolve('public.pem'), 'utf8')
-const key = new NodeRSA(publicKey, 'pkcs8-public', {
-  encryptionScheme: 'pkcs1',
-})
+const publicKey = fs.readFileSync(resolve("public.pem"), "utf8");
+const key = new NodeRSA(publicKey, "pkcs8-public", {
+  encryptionScheme: "pkcs1",
+});
 
 const api = {
-  login: 'https://changjiang.yuketang.cn/pc/login/verify_pwd_login/',
-  getOnLessonData: 'https://changjiang.yuketang.cn/v/course_meta/on_lesson_courses',
-  attendLesson: 'https://changjiang.yuketang.cn/v/lesson/lesson_info_v2',
-}
+  login: "https://changjiang.yuketang.cn/pc/login/verify_pwd_login/",
+  getOnLessonData:
+    "https://changjiang.yuketang.cn/v/course_meta/on_lesson_courses",
+  attendLesson: "https://changjiang.yuketang.cn/v/lesson/lesson_info_v2",
+};
 
 const login = async (username, password) => {
   const body = await customGot(api.login, {
-    method: 'POST',
+    method: "POST",
     json: {
-      type: 'PP',
+      type: "PP",
       name: username,
-      pwd: key.encrypt(password, 'base64'),
+      pwd: key.encrypt(password, "base64"),
     },
-  }).json()
-  if (!body.success) throw new Error('login failed')
-}
+  }).json();
+  if (!body.success) throw new Error("login failed");
+};
 
 const getOnLessonInfo = async () => {
   const {
     data: { on_lessons },
-  } = await customGot(api.getOnLessonData).json()
-  return on_lessons.length > 0 ? on_lessons[0] : false
-}
+  } = await customGot(api.getOnLessonData).json();
+  return on_lessons.length > 0 ? on_lessons : false;
+};
 
 const attendLesson = async ({
   lesson_id,
@@ -59,46 +60,46 @@ const attendLesson = async ({
   // await customGot(`https://changjiang.yuketang.cn/lesson/fullscreen/${lesson_id}?source=5`)
   const data = await customGot(api.attendLesson, {
     searchParams: { lesson_id },
-  }).json()
+  }).json();
   const { success } = data;
   if (success) {
-    console.log('Success: ', name);
-    if(!successLessons.has(lesson_id)) {
-      sendNotify('YuKeTang: success', name)
+    console.log("Success: ", name);
+    if (!successLessons.has(lesson_id)) {
+      sendNotify("YuKeTang: success", name);
       successLessons.add(lesson_id);
     }
   } else {
-    console.log('Error: ', data);
-    sendNotify('YukeTang: Error', JSON.stringify(data, null, 2))
+    console.log("Error: ", data);
+    sendNotify("YukeTang: Error", JSON.stringify(data, null, 2));
   }
-}
+};
 
 const execCheckIn = async () => {
-  console.log(`Number of executions: ${++count}`)
-  const lessonInfo = await getOnLessonInfo()
-  if(count >= times) {
-    sendNotify('YukeTang: End', new Date().toLocaleString('zh-CN'))
-    return
+  console.log(`Number of executions: ${++count}`);
+  const lessonInfo = await getOnLessonInfo();
+  if (count >= times) {
+    sendNotify("YukeTang: End", new Date().toLocaleString("zh-CN"));
+    return;
   }
-  if(count < times) {
-    setTimeout(execCheckIn, 1000 * 20)
+  if (count < times) {
+    setTimeout(execCheckIn, 1000 * 20);
   }
   if (!lessonInfo) {
-    return
+    return;
   }
-  attendLesson(lessonInfo)
-}
+  lessonInfo.forEach((lesson) => attendLesson(lesson));
+};
 
 const startUp = async () => {
-  const { USER_INFO } = process.env
-  const [USERNAME, PASSWORD] = USER_INFO.split('|')
+  const { USER_INFO } = process.env;
+  const [USERNAME, PASSWORD] = USER_INFO.split("|");
   if (!(USERNAME && PASSWORD)) {
-    console.log('Failed: USER_INFO not provided.')
-    process.exit(0)
+    console.log("Failed: USER_INFO not provided.");
+    process.exit(0);
   }
-  await login(USERNAME, PASSWORD)
-  sendNotify('YukeTang: Start', new Date().toLocaleString('zh-CN'))
-  execCheckIn()
-}
+  await login(USERNAME, PASSWORD);
+  sendNotify("YukeTang: Start", new Date().toLocaleString("zh-CN"));
+  execCheckIn();
+};
 
-startUp()
+startUp();
