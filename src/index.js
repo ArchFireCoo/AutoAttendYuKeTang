7ebfbede27423f5ec71d2f1ec374825fdd912e1a
arrayJY <arrayJY@outlook.com>
2021-09-22 19:15:16 +0800

    update for v3 api.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const resolve = function (...args) {
 };
 
 let count = 0;
-const times = 960;
 
 const cookieJar = new CookieJar();
 
@@ -33,6 +32,7 @@ const api = {
   getOnLessonData:
     "https://changjiang.yuketang.cn/v/course_meta/on_lesson_courses",
   attendLesson: "https://changjiang.yuketang.cn/v/lesson/lesson_info_v2",
+  attendLessonV3: "https://changjiang.yuketang.cn/api/v3/classroom/basic-info",
   getOnLessonDataV3:
     "https://changjiang.yuketang.cn/api/v3/classroom/on-lesson",
 };
@@ -53,23 +53,45 @@ const getOnLessonInfo = async () => {
   const {
     data: { on_lessons: on_lessons_v2 },
   } = await customGot(api.getOnLessonData).json();
-  const {
-    data: { onLessonClassrooms },
-  } = await customGot(api.getOnLessonDataV3).json();
-  const on_lessons = on_lessons_v2?.concat(onLessonClassrooms ?? []);
+  const on_lessons = on_lessons_v2;
   return on_lessons && on_lessons.length > 0 ? on_lessons : false;
 };
 
-const attendLesson = async ({
-  lesson_id,
-  name,
-  classroom,
-}) => {
-  // await customGot(`https://changjiang.yuketang.cn/lesson/fullscreen/${lesson_id}?source=5`)
+const getOnLessonInfoV3 = async () => {
+  const {
+    data: { onLessonClassrooms },
+  } = await customGot(api.getOnLessonDataV3).json();
+  return onLessonClassrooms && onLessonClassrooms.length > 0
+    ? onLessonClassrooms
+    : false;
+};
+
+const attendLessonV3Request = (classroom_id) =>
+  customGot(api.attendLessonV3, {
+    searchParams: { classroom_id },
+  }).json();
+
+const attendLessonV3 = async ({ classroomId, courseName }) => {
+  const data = await attendLessonV3Request(classroomId);
+  const {code} = data
+  const success = code === 0
+  if (success) {
+    console.log("Success: ", courseName);
+    if (!successLessons.has(classroomId)) {
+      sendNotify("YuKeTang: success", courseName);
+      successLessons.add(classroomId);
+    }
+  } else {
+    console.log("Error: ", data);
+    sendNotify("YukeTang: Error", JSON.stringify(data, null, 2));
+  }
+};
+
+const attendLesson = async ({ lesson_id, classroom }) => {
   const data = await customGot(api.attendLesson, {
     searchParams: { lesson_id },
   }).json();
-  name = classroom?.course?.name ?? name
+  const name = classroom?.course?.name;
   const { success } = data;
   if (success) {
     console.log("Success: ", name);
@@ -92,20 +114,23 @@ const execCheckIn = async () => {
     if (nowTime.diff(startTime, "minute") > 320) {
       sendNotify("YukeTang: End", nowTime.format("YYYY-MM-DD hh:mm:ss"));
       return;
-    } 
+    }
   }
   setTimeout(execCheckIn, 1000 * 20);
   let lessonInfo = undefined;
+  let lessonInfoV3 = undefined;
   try {
     lessonInfo = await getOnLessonInfo();
+    lessonInfoV3 = await getOnLessonInfoV3();
   } catch (err) {
     console.log("GetOnInfo Failed:", err);
   }
-  if (!lessonInfo) {
+  if (!lessonInfo && !lessonInfoV3) {
     return;
   }
   try {
-    lessonInfo.forEach((lesson) => attendLesson(lesson));
+    lessonInfo && lessonInfo.forEach((lesson) => attendLesson(lesson));
+    lessonInfoV3 && lessonInfoV3.forEach((lesson) => attendLessonV3(lesson));
   } catch (err) {
     console.log("Attend Failed:", err);
   }
