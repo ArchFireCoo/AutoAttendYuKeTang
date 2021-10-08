eabc3dfe908cee15ede40ecf4655a8cf97bfdf11
hibarix <hibarix@163.com>
2021-10-08 15:40:45 +0800

    temp

--- src/sendNotify.js
+++ src/sendNotify.js
@@ -6,9 +6,11 @@ const BARKID = process.env.BARK_ID
 const sendNotify = (title, message) => {
   console.log("SCKEY:" + SCKEY);
   console.log("BARKID:" + BARKID);
+  console.log("title:" + title);
+  console.log("message:" + message);
   console.log("BARKReqURL:" + `https://api.day.app/${BARKID}/${title}/${message}`);
   if (BARKID) {
-    got(`https://api.day.app/${BARKID}/${title}/${message}`, {
+    got(`https://api.day.app/${BARKID}/%E6%A0%87%E9%A2%98/%E5%86%85%E5%AE%B9?group=%E9%95%BF%E6%B1%9F%E4%B8%8E%E8%AF%BE%E5%A0%82`, {
           responseType: 'json'  
     }).then( ({ body }) => {
       if (body.code === 200) {
