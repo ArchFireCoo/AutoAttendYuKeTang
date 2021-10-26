38389a30bd250153ec065a7b793c6febf3b5e7c3
arrayJY <ji957455952@163.com>
2021-10-26 13:21:43 +0800

    fix v3 api cannot checkin.

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ let startTime = undefined;
 
 const customGot = got.extend({
   cookieJar,
-  timeout: 10000
+  timeout: 10000,
 });
 
 const successLessons = new Set();
@@ -33,7 +33,7 @@ const api = {
   getOnLessonData:
     "https://changjiang.yuketang.cn/v/course_meta/on_lesson_courses",
   attendLesson: "https://changjiang.yuketang.cn/v/lesson/lesson_info_v2",
-  attendLessonV3: "https://changjiang.yuketang.cn/api/v3/classroom/basic-info",
+  attendLessonV3: "https://changjiang.yuketang.cn/api/v3/lesson/checkin",
   getOnLessonDataV3:
     "https://changjiang.yuketang.cn/api/v3/classroom/on-lesson",
 };
@@ -67,15 +67,19 @@ const getOnLessonInfoV3 = async () => {
     : false;
 };
 
-const attendLessonV3Request = (classroom_id) =>
+const attendLessonV3Request = (lessonId) =>
   customGot(api.attendLessonV3, {
-    searchParams: { classroom_id },
+    method: "POST",
+    json: {
+      source: 1,
+      lessonId,
+    },
   }).json();
 
-const attendLessonV3 = async ({ classroomId, courseName }) => {
-  const data = await attendLessonV3Request(classroomId);
-  const {code} = data
-  const success = code === 0
+const attendLessonV3 = async ({ lessonId, classroomId, courseName }) => {
+  const data = await attendLessonV3Request(lessonId);
+  const { code } = data;
+  const success = code === 0;
   if (success) {
     console.log("Success: ", courseName);
     if (!successLessons.has(classroomId)) {
